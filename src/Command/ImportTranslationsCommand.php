8f901781f9e6ccafd1cdaded8eb8107546e4af11
Warxcell <warxcell@gmail.com>
2022-01-14 17:21:30 +0200

    allow importing only specific catalogues

--- src/Command/ImportTranslationsCommand.php
+++ src/Command/ImportTranslationsCommand.php
@@ -9,8 +9,13 @@ use Arxy\TranslationsBundle\Repository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Translation\MessageCatalogue;
 use Symfony\Component\Translation\TranslatorBagInterface;
+use Throwable;
+
+use function sprintf;
 
 class ImportTranslationsCommand extends Command
 {
@@ -32,18 +37,54 @@ class ImportTranslationsCommand extends Command
         $this->entityManager = $entityManager;
     }
 
+    protected function configure(): void
+    {
+        $this->addOption('catalogue', 'c', InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED);
+    }
+
+    /**
+     * @throws Throwable
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $repository = $this->entityManager->getRepository(Language::class);
 
-        /** @var Language[] $languages */
-        $languages = $repository->findAll();
-        foreach ($languages as $language) {
-            $this->repository->persistCatalogue($this->translatorBag->getCatalogue($language->getLocale()));
-            $output->writeln('Imported catalogue ' . $language->getLocale());
+        $this->entityManager->beginTransaction();
+
+        try {
+            /** @var Language[] $languages */
+            $languages = $repository->findAll();
+
+            foreach ($languages as $language) {
+                $finalCatalogue = $this->translatorBag->getCatalogue($language->getLocale());
+
+                if ($input->hasOption('catalogue')) {
+                    $catalogue = $finalCatalogue;
+                    $finalCatalogue = new MessageCatalogue($language->getLocale());
+                    foreach ($input->getOption('catalogue') as $catalogueAsString) {
+                        $output->writeln(
+                            sprintf(
+                                'Importing from language %s catalogue %s',
+                                $language->getLocale(),
+                                $catalogueAsString
+                            )
+                        );
+
+                        $finalCatalogue->add($catalogue->all($catalogueAsString), $catalogueAsString);
+                    }
+                }
+
+                $output->writeln(sprintf('Importing from language %s', $language->getLocale()));
+
+                $this->repository->persistCatalogue($finalCatalogue);
+            }
+
+            $this->entityManager->commit();
+        } catch (Throwable $throwable) {
+            $this->entityManager->rollback();
+            throw $throwable;
         }
 
         return 0;
     }
 }
-
