20939a07e199fcaf319557e2cb37235f14f18bd1
Warxcell <warxcell@gmail.com>
2022-01-14 15:43:56 +0200

    Special command to import translations.

--- src/Command/ImportTranslationsCommand.php
+++ src/Command/ImportTranslationsCommand.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Arxy\TranslationsBundle\Command;
 
+use Arxy\TranslationsBundle\Model\Language;
 use Arxy\TranslationsBundle\Repository;
+use Doctrine\Persistence\ManagerRegistry;
+use RuntimeException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -17,18 +20,33 @@ class ImportTranslationsCommand extends Command
 
     private Repository $repository;
     private TranslatorBagInterface $translatorBag;
+    private ManagerRegistry $managerRegistry;
 
-    public function __construct(Repository $repository, TranslatorBagInterface $translatorBag)
-    {
+    public function __construct(
+        Repository $repository,
+        TranslatorBagInterface $translatorBag,
+        ManagerRegistry $managerRegistry
+    ) {
         parent::__construct();
         $this->repository = $repository;
         $this->translatorBag = $translatorBag;
+        $this->managerRegistry = $managerRegistry;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->repository->persistCatalogue($this->translatorBag->getCatalogue());
-        $output->writeln('Imported catalogue');
+        $manager = $this->managerRegistry->getManagerForClass(Language::class);
+        if ($manager === null) {
+            throw new RuntimeException('No manager found for ' . Language::class);
+        }
+        $repository = $manager->getRepository(Language::class);
+
+        /** @var Language[] $languages */
+        $languages = $repository->findAll();
+        foreach ($languages as $language) {
+            $this->repository->persistCatalogue($this->translatorBag->getCatalogue($language->getLocale()));
+            $output->writeln('Imported catalogue ' . $language->getLocale());
+        }
 
         return 0;
     }
