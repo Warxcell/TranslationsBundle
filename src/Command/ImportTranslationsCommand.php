608b6d35bbba30dab34b781a74f8ceff35cf3811
Warxcell <warxcell@gmail.com>
2022-01-14 15:04:08 +0200

    Special command to import translations.

---
+++ src/Command/ImportTranslationsCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arxy\TranslationsBundle\Command;
+
+use Arxy\TranslationsBundle\Repository;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Translation\TranslatorBagInterface;
+
+class ImportTranslationsCommand extends Command
+{
+    private Repository $repository;
+    private TranslatorBagInterface $translatorBag;
+
+    public function __construct(Repository $repository, TranslatorBagInterface $translatorBag)
+    {
+        parent::__construct();
+        $this->repository = $repository;
+        $this->translatorBag = $translatorBag;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        foreach ($this->translatorBag->getCatalogues() as $catalogue) {
+            $this->repository->persistCatalogue($catalogue);
+        }
+
+        return 0;
+    }
+}
+
